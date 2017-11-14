d7f45c968a824154bdef497308a5387d79e34fe1
djk2120 <djk2120@columbia.edu>
2017-11-14 11:20:21 -0500

    remove numpy dependency

--- unitconvert/tests/test_unitconvert.py
+++ unitconvert/tests/test_unitconvert.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pytest
 
 from unitconvert import temperature
@@ -6,24 +5,24 @@ from unitconvert import distance
 
 def test_fahrenheit_to_celsius():
     should_be_10 = temperature.fahrenheit_to_celsius(50)
-    np.testing.assert_allclose(should_be_10, 10, rtol=1e-5)
+    assert round(should_be_10,4)==10
     negative_40  = temperature.fahrenheit_to_celsius(-40)
-    np.testing.assert_allclose(negative_40,-40, rtol=1e-5)
+    assert round(negative_40,4)==-40
 
 def test_celsius_to_fahrenheit():
     should_be_50 = temperature.celsius_to_fahrenheit(10)
-    np.testing.assert_allclose(should_be_50, 50, rtol=1e-5)
+    assert round(should_be_50,4)==50
     negative_40  = temperature.celsius_to_fahrenheit(-40)
-    np.testing.assert_allclose(negative_40,-40, rtol=1e-5)
+    assert round(negative_40,4)==-40
 
 def test_miles_to_kilometers():
     should_be_0 = distance.miles_to_kilometers(0)
-    np.testing.assert_allclose(should_be_0, 0, rtol=1e-5)
+    assert round(should_be_0,4)==0
     about_5k    = distance.miles_to_kilometers(3.1)
-    np.testing.assert_allclose(about_5k, 5, rtol=1e-1)
+    assert round(about_5k,1)==5
 
 def test_kilometers_to_miles():
     should_be_0 = distance.kilometers_to_miles(0)
-    np.testing.assert_allclose(should_be_0, 0, rtol=1e-5)
+    assert round(should_be_0,4)==0
     marathon    = distance.kilometers_to_miles(42.195)
-    np.testing.assert_allclose(marathon, 26.2, rtol=1e-1)
+    assert round(marathon,1)==26.2
