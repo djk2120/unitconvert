6536d6fdd4d27678b94aabaf7319cd9c8bc0f2ba
djk2120 <djk2120@columbia.edu>
2017-11-14 10:33:03 -0500

    implement temperature conversions

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = "unitconvert",
+    version = "0.0.1",
+    author = "Ryan Abernathey",
+    packages=['unitconvert'],
+    install_requires=['numpy'],
+)
---
+++ unitconvert/temperature.py
@@ -0,0 +1,10 @@
+def fahrenheit_to_celsius(temperature_in_f):
+    temperature_in_c = 5/9*(temperature_in_f-32)
+    print(temperature_in_c)
+    return temperature_in_c
+
+def celsius_to_fahrenheit(temperature_in_c):
+    temperature_in_f = 9/5*temperature_in_c+32
+    print(temperature_in_f)
+    return temperature_in_f
+
